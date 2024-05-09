14611fd84afbda9da37babecc01e2cb9bcdd0320
palmuccivalerio <palmuccivalerio@gmail.com>
2024-05-09 17:25:42 +0200

    add:flaglang

--- src/App.vue
+++ src/App.vue
@@ -22,17 +22,44 @@ export default {
           api_key: this.store.apiKey,
         },
       }).then((resp) => {
-        console.log(resp);
+        console.log(resp.data.results[0].original_title);
+        console.log(resp.data.results[0].title);
+        this.store.filmArr= resp.data.results
+
       });
 
 
     },
+    getImgUrl(name){
+      return new URL (`./assets/img/${name}.png`,import.meta.url).href
+    },
+    functionFlag(lang){
+      let inArray = false 
+      this.store.flag.forEach(curLang => {
+        if (curLang === lang ){
+          inArray = true
+          console.log(curLang,lang);
+        }
+        console.log(inArray);
+      });
+      return inArray
+    }
   },
 };
 </script>
 
 <template>
   <AppSearch @nameTitle="search" />
+  <ul>
+    <li v-for="curFilm in this.store.filmArr">
+      {{curFilm.title}} 
+      {{curFilm.original_title}} 
+      {{curFilm.vote_average}} 
+      <img v-if="functionFlag(curFilm.original_language)" :src="getImgUrl(curFilm.original_language)" alt="">
+      <p v-else> {{curFilm.original_language}}</p>
+    </li>
+    
+  </ul>
 </template>
 
 <style></style>
--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
 import {reactive} from "vue";
 export const store= reactive ({
     apiKey:"63db7b8ff02dda950e461fda09d9184f",
-    searched:""
+    searched:"",
+    filmArr: [],
+    flag: ["de", "en","it","ja"],
 
 });
