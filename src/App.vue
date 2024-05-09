854949111e7846a0f146645aa12a57e01afefbff
palmuccivalerio <palmuccivalerio@gmail.com>
2024-05-09 12:52:52 +0200

    add:call

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import AppSearch from "./components/AppSearch.vue";
+import { store } from "./store.js";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  components: {
+    AppSearch,
+  },
+
+  methods: {
+    search() {
+      axios.get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+          query: this.store.searched,
+          api_key: this.store.apiKey,
+        },
+      }).then((resp) => {
+        console.log(resp);
+      });
+
+
+    },
+  },
+};
+</script>
+
+<template>
+  <AppSearch @nameTitle="search" />
+</template>
+
+<style></style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,24 @@
+<script>
+import { store } from "../store.js";
+export default {
+    data(){
+        return{
+            store 
+        }
+    }
+}
+
+</script>
+
+<template>
+<input type="text" placeholder="scrivi film" v-model="this.store.searched">
+<button @click="$emit('nameTitle')" > cerca </button>
+
+
+</template>
+
+
+
+<style>
+
+</style>
