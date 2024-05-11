6a273804bffe4b0a3ced59816f2273f674d571dc
palmuccivalerio <palmuccivalerio@gmail.com>
2024-05-11 17:43:16 +0200

    add:layout

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@ export default {
         })
         .then((resp) => {
           this.store.tvArr = resp.data.results;
-          console.log(this.store.tvArr)
+          console.log(this.store.tvArr);
         });
     },
     getImgUrl(name) {
@@ -50,45 +50,133 @@ export default {
       });
       return inArray;
     },
+    starIcon(num) {
+      console.log(Math.round(num / 2));
+      return Math.round(num / 2);
+    },
   },
 };
 </script>
 
 <template>
   <AppSearch @nameTitle="search" />
-  <h2>FILM</h2>
-  <ul>
-    <li v-for="curFilm in this.store.filmArr">
-      {{ curFilm.title }}
-      {{ curFilm.original_title }}
-      {{ curFilm.vote_average }}
-      <span v-for="x in 5">s</span>
-      <img :src="'http://image.tmdb.org/t/p/w500/' + curFilm.poster_path" alt="">
+   <div class="results-list">
+    <h2 class="title">FILM</h2>
+    <section class="movies">
+      <div v-for="curFilm in this.store.filmArr" class="card">
+        <div class="card-back">
+          <h3>{{ curFilm.title }}</h3>
+          <h6>{{ curFilm.original_title }}</h6>
+          <img
+            v-if="functionFlag(curFilm.original_language)"
+            :src="getImgUrl(curFilm.original_language)"
+            alt=""
+          />
+          <p v-else>{{ curFilm.original_language }}</p>
+          <div class="vote">
+            <i
+              class="fas fa fa-solid fa-star"
+              v-for="star in starIcon(curFilm.vote_average)"
+            ></i>
+            <i
+              class="fa-regular fa-star"
+              v-for="star in 5 - starIcon(curFilm.vote_average)"
+            ></i>
+          </div>
+        </div>
+        <div class="card-front">
+          <img
+            :src="`https://image.tmdb.org/t/p/w342/${curFilm.poster_path}`"
+            alt="curFilm.title"
+          />
+        </div>
+      </div>
+    </section>
 
-      <img
-        v-if="functionFlag(curFilm.original_language)"
-        :src="getImgUrl(curFilm.original_language)"
-        alt=""
-      />
-      <p v-else>{{ curFilm.original_language }}</p>
-    </li>
-  </ul>
-  <H2>SERIE TV</H2>
-  <ul>
-    <li v-for= " curSeries in this.store.tvArr">
-      {{curSeries.name}}
-      {{curSeries.original_name}}    
-      {{curSeries.vote_average}}
-      <img :src="'http://image.tmdb.org/t/p/w500/' + curSeries.poster_path" alt="">
-      <img
-        v-if="functionFlag(curSeries.original_language)"
-        :src="getImgUrl(curSeries.original_language)"
-        alt=""
-      />
-      <p v-else>{{ curSeries.original_language }}</p>
-  
-    </li>
-  </ul>
+    <H2 class="title">SERIE TV</H2>
+
+    <section class="movies">
+      <div v-for="curSeries in this.store.tvArr" class="card">
+        <div class="card-back">
+          <h3>{{ curSeries.name }}</h3>
+          <h6>{{ curSeries.original_name }}</h6>
+          <img
+            v-if="functionFlag(curSeries.original_language)"
+            :src="getImgUrl(curSeries.original_language)"
+            alt=""
+          />
+          <p v-else>{{ curSeries.original_language }}</p>
+          <div class="vote">
+            <i
+              class="fas fa fa-solid fa-star"
+              v-for="star in starIcon(curSeries.vote_average)"
+            ></i>
+            <i
+              class="fa-regular fa-star"
+              v-for="star in 5 - starIcon(curSeries.vote_average)"
+            ></i>
+          </div>
+        </div>
+        <div class="card-front">
+          <img
+            :src="`https://image.tmdb.org/t/p/w342/${curSeries.poster_path}`"
+            alt="curSeries.title"
+          />
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+.results-list {
+  padding: 20px;
+img{
+  max-width: 100%;
+  max-height: 100%;
+}
+
+  .title{
+    color: red;
+    font-size:xx-large;
+  }
+
+  .movies,
+  .series {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
+
+  .card {
+    border: 20px;
+    color: white;
+    height: 450px;
+    width: calc(100% / 5 - 20px);
+
+    &:hover {
+      .card-back {
+        display: block;
+      }
+
+      .card-front {
+        display: none;
+      }
+    }
+
+    .card-back {
+      display: none;
+      padding: 20px;
+
+      .flag {
+        width: 10px;
+      }
+
+      .card-front {
+        height: 300px;
+        margin: 20px;
+      }
+    }
+  }
+}
+</style>
--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -14,4 +14,25 @@ export default {
   <button @click="$emit('nameTitle')">cerca</button>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+input {
+  width: 80%;
+  background-color: white;
+  border-radius: 15px ;
+  border: 2px solid black;
+  padding: 12px 16px;
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+}
+
+button {
+  width: 20%;
+  border: 2px solid black;
+  border-radius: 15px ; 
+  padding: 12px 16px;
+  background-color: red;
+  border-top-right-radius: 3px;
+  border-bottom-right-radius: 3px;
+  cursor: pointer;
+}
+</style>
